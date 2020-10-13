b7ca038c5650111f28510d34fd4643a702d83866
Chris Stockdale <cjsgamingnetwork92@gmail.com>
2020-10-13 03:52:26 -0500

    Initial Commit

---
+++ SimpleCalculator/src/com/CSDEV_CREATIONS/SimpleCalculator.java
@@ -0,0 +1,46 @@
+package com.CSDEV_CREATIONS;
+
+public class SimpleCalculator {
+
+    public double firstNumber;
+    public double secondNumber;
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getFirstNumber() {
+        return this.firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getSecondNumber() {
+        return this.secondNumber;
+    }
+
+    public double getAdditionResult() {
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult() {
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult() {
+        if(secondNumber == 0) {
+            return 0;
+        } else {
+            return firstNumber / secondNumber;
+        }
+
+
+    }
+
+}
